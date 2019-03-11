accca75ded6c597710ae26aecfa646b291a68eb6
lx <838327609@qq.com>
2019-03-11 23:16:18 +0800

    feat(location):添加多线程的实现方法

--- src/main/java/com/study/luxin/App.java
+++ src/main/java/com/study/luxin/App.java
@@ -2,23 +2,62 @@ package com.study.luxin;
 
 import org.junit.Test;
 
+import java.util.concurrent.*;
+
 /**
  * Hello world!
  */
 public final class App {
-    public static void main(String[] args) {
 
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("thread run()");
 
-        System.out.println("Hello World!");
+        }
+    }
+
+    class MyRunnable implements Runnable{
+        @Override
+        public void run() {
+            System.out.println("runnable run()");
+        }
+    }
+
+    class MyCallable implements Callable{
+        @Override
+        public Object call() throws Exception {
+            System.out.println("callable run()");
+            return "abc";
+        }
     }
 
 
     @Test
-    public void test(){
+    public void threadTest() throws InterruptedException, ExecutionException {
+        Thread thread = new MyThread();
+        thread.start();
+        ExecutorService executorService =  new ThreadPoolExecutor(1,1,100L, TimeUnit.SECONDS, new LinkedBlockingQueue());
 
-        System.out.println(ClassFieldLoadOrder.b);
+        //
+        Runnable runnable = new MyRunnable();
+        executorService.execute(runnable);
 
+        //
+        Callable callable = new MyCallable();
+        Future submit1 = executorService.submit(callable);
 
+        System.out.println(submit1.get());
+
+        //
+        FutureTask<Object> futureTask = new FutureTask(callable);
+        Future<?> submit = executorService.submit(futureTask);
+
+        TimeUnit.SECONDS.sleep(1);
+
+        System.out.println(submit.get());
+
+        TimeUnit.SECONDS.sleep(11);
     }
 
 
