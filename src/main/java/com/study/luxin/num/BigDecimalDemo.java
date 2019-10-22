66b2304c306d2a7104798980abff4aee5d9131f9
luxin <838327609.qq.com>
2019-10-22 16:55:48 +0800

    feature: bigDecimal取精度,及进位 测试

--- src/main/java/com/study/luxin/num/BigDecimalDemo.java
+++ src/main/java/com/study/luxin/num/BigDecimalDemo.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ROUND_HALF_UP;
+
 public class BigDecimalDemo {
 
     @Test
@@ -11,7 +13,7 @@ public class BigDecimalDemo {
      * stripTrailingZeros删除无用的0的方法.
      */
     public void removeUnusedZero() {
-        BigDecimal v1 = new BigDecimal("264,000");
+        BigDecimal v1 = new BigDecimal("264.000");
         BigDecimal v2 = new BigDecimal("1.234");
 
         v1 = v1.stripTrailingZeros();
@@ -20,4 +22,16 @@ public class BigDecimalDemo {
     }
 
 
+    @Test
+    /**
+     * 处理精度问题,且四舍五入
+     */
+    public void decimalPlaces() {
+        //注意 不要用double初始化 bigDecimal因为double是不准确的,1.5d=1.49999999999,会导致四舍五入不进位
+        String numStr = "1234.1235";
+        BigDecimal bigDecimal = new BigDecimal(numStr);
+        BigDecimal bigDecimal1 = bigDecimal.setScale(3, ROUND_HALF_UP);
+
+        assert bigDecimal1.toString().equals("1234.124");
+    }
 }
