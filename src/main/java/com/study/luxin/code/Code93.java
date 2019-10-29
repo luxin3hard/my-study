892e0c9f2c682cc2d8d869bac3ce404b47845426
lx <838327609@qq.com>
2019-10-29 23:12:17 +0800

    增加解题

--- src/main/java/com/study/luxin/code/Code93.java
+++ src/main/java/com/study/luxin/code/Code93.java
@@ -8,7 +8,7 @@ import java.util.*;
 //https://leetcode-cn.com/problems/restore-ip-addresses/
 public class Code93 {
 
-    public List<String> restoreIpAddresses(String s) {
+    public List<String> restoreIpAddresses1(String s) {
         Set<Integer> set = new HashSet<>();
         set.add(3333);
         int l = s.length();
@@ -39,9 +39,50 @@ public class Code93 {
 
         //restoreIpAddresses("123456789010");
 
-        restoreIpAddresses("1234567890");
+        restoreIpAddresses1("1234567890");
 
 
+    }
+
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>(); //保存最终的所有结果
+        getAns(s, 0, new StringBuilder(), ans, 0);
+        return ans;
+    }
+
+    private void getAns(String s, int start, StringBuilder tmp, List<String> ans, int count) {
+        if (s.length() - start > 3 * (4 - count)) {
+            tmp.delete(0, start + count);
+            return;
+        }
+
+
+        for (int i = 1; i <= 3; i++) {
+            String str = s.substring(start, start + i);
+
+            if (Integer.valueOf(str) > 255 || str.startsWith("0")) {
+                return;
+            }
+
+            if (count < 3) {
+                tmp.append(str + ".");
+            } else {
+                tmp.append(str);
+            }
+            getAns(s, start + i, tmp, ans, count + 1);
+        }
+
+        if (count == 3) {
+            ans.add(tmp.toString());
+            tmp.delete(0, Integer.MAX_VALUE);
+        }
+    }
+
+
+    @Test
+    public void test1() {
+        restoreIpAddresses("123123123123");
     }
 
 
