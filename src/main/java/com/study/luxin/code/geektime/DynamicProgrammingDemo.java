46f9e9c21f6156584d9e51afa188eb65ba6bd62f
luxin <838327609.qq.com>
2019-12-16 19:35:07 +0800

    feature:动态规划提交

--- src/main/java/com/study/luxin/code/geektime/DynamicProgrammingDemo.java
+++ src/main/java/com/study/luxin/code/geektime/DynamicProgrammingDemo.java
@@ -90,4 +90,69 @@ public class DynamicProgrammingDemo {
     }
 
 
+    public int lvstLong(String a, String b) {
+
+        int[][] m = new int[a.length()][b.length()];
+
+        char at = a.charAt(0);
+        char bt = b.charAt(0);
+        for (int i = 0; i < b.length(); i++) {
+            if (at != b.charAt(i)) {
+                if (i == 0) {
+                    m[0][i] = 1;
+                } else {
+                    m[0][i] = m[0][i - 1] + 1;
+                }
+            }
+        }
+
+        for (int i = 1; i < a.length(); i++) {
+            if (bt != a.charAt(i)) {
+                m[i][0] = m[i - 1][0] + 1;
+            }
+        }
+
+        for (int i = 1; i < a.length(); i++) {
+            for (int j = 1; j < b.length(); j++) {
+                int t;
+                if (a.charAt(i) == b.charAt(j)) {
+                    t = m[i - 1][j - 1];
+                } else {
+                    t = m[i - 1][j - 1] + 1;
+                }
+                m[i][j] = min(min(m[i - 1][j] + 1, m[i][j - 1] + 1), t);
+            }
+        }
+        return m[a.length() - 1][b.length() - 1];
+    }
+
+    @Test
+    public void lvstLongTest() {
+        System.out.println(lvstLong("mitcmu", "mitcnu"));
+    }
+
+
+    public int maxLengthAscNums(int[] nums) {
+        int[] m = new int[nums.length];
+        m[0] = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            m[i] = 1;
+            // 可以建跳表,快速定位小于nums[i] 的数据
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] <= nums[i]) {
+                    max = Math.max(m[j] + 1, max);
+                    m[i] = max;
+                }
+            }
+        }
+        return max;
+    }
+
+    @Test
+    public void maxLengthAscNums() {
+        System.out.println(maxLengthAscNums(new int[]{2, 9, 3, 6, 5, 1, 7}));
+    }
+
+
 }
